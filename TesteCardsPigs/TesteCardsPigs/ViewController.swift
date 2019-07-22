bb1d7d6603dda5cc819b516523fb52b24a70a5ed
Guilherme Enes <guilherme.epecci@gmail.com>
2019-07-22 02:20:20 -0300

    Working on notifications

--- TesteCardsPigs/TesteCardsPigs/ViewController.swift
+++ TesteCardsPigs/TesteCardsPigs/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // codigo para notificação
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (accepted, error) in
+            // caso queira tratar do erro
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Teste"
+        content.body = "Cuí?"
+        
+        let date = Date().addingTimeInterval(10)
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        let uuiString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuiString, content: content, trigger: trigger)
+        
+        center.add(request) { (error) in
+            // caso queira tratar algum erro
+        }
     }
 
 
