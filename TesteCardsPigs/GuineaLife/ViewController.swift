54aefa5998c76f0c13f07b3a424040c33e22800f
Guilherme Enes <guilherme.epecci@gmail.com>
2019-07-30 17:07:34 -0300

    Notifications fixed

--- TesteCardsPigs/GuineaLife/ViewController.swift
+++ TesteCardsPigs/GuineaLife/ViewController.swift
@@ -21,21 +21,41 @@ class ViewController: UIViewController {
         }
         
         let msg = UNMutableNotificationContent()
-        msg.title = "Alôô, alguem ai?"
-        msg.body = "Estou com fome.. cuí"
+        msg.title = "Aiai que soninho"
+
+        msg.body = "Cade minhas comidaa! AAAA cui cuii!"
         
-        var date = DateComponents() //notificacao de comida
-        date.hour = 6 // 6 horas da manhã
-        date.minute = 00 // 0 minutos
+        var manha = DateComponents() //notificacao de comida
+        manha.hour = 07 // 6 horas da manhã
+        manha.minute = 00 // 0 minutos
         
-        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false) //repete todos os dias
+        let trigger = UNCalendarNotificationTrigger(dateMatching: manha, repeats: false) //repete todos os dias
         
         let uuiString = UUID().uuidString
         let request = UNNotificationRequest(identifier: uuiString, content: msg, trigger: trigger)
         
+        
+        let msg2 = UNMutableNotificationContent()
+        msg2.title = "Alôô, alguem ai?"
+        msg2.body = "Estou com fome.. cuí"
+        
+        var almoco = DateComponents()
+        almoco.hour = 11
+        almoco.minute = 00
+        
+        let almocotrigger = UNCalendarNotificationTrigger(dateMatching: almoco, repeats: false)
+        
+        let uuiString2 = UUID().uuidString
+        let request2 = UNNotificationRequest(identifier: uuiString2, content: msg2, trigger: almocotrigger)
+        
+        
+        center.add(request2) { (error) in
+            // caso queira tratar algum erro
+        }     // notificacao do almoço
+        
         center.add(request) { (error) in
             // caso queira tratar algum erro
-        }
+        }     // notificacao da manhã
     }
     
     // tela Tamanho cercado
